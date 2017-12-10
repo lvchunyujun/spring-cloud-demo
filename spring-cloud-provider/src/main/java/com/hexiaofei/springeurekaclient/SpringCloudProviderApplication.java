653de5209747467caa0ef769e7a0f54ccc1f1cfe
lcyj <1097957468@qq.com>
2017-12-10 23:59:17 +0800

    修改  spring-cloud-provider 配置文件

--- spring-cloud-provider/src/main/java/com/hexiaofei/springeurekaclient/SpringCloudProviderApplication.java
+++ spring-cloud-provider/src/main/java/com/hexiaofei/springeurekaclient/SpringCloudProviderApplication.java
@@ -1,12 +1,12 @@
 package com.hexiaofei.springeurekaclient;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.environment.Environment;
 
 @MapperScan("com.hexiaofei.springeurekaclient.dao")
-@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringCloudProviderApplication {
 
--- spring-cloud-provider/src/main/java/com/hexiaofei/springeurekaclient/web/UserController.java
+++ spring-cloud-provider/src/main/java/com/hexiaofei/springeurekaclient/web/UserController.java
@@ -5,11 +5,15 @@ import com.hexiaofei.springeurekaclient.service.IUserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.config.environment.Environment;
+import org.springframework.cloud.config.environment.PropertySource;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -21,19 +25,24 @@ public class UserController {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 
+    @Value("${key}")
+    private String servername;
+
+    @Autowired
+    private IUserService userService;
+
     @RequestMapping("/{id}")
     @ResponseBody
     public User findUserById(@PathVariable Integer id){
         User user = userService.getUserById(id);
+
         try {
             TimeUnit.MILLISECONDS.sleep(100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        LOGGER.info("查询客户id={},详情{}",id,user);
+        LOGGER.info("【{}】查询客户id={},详情{}",servername,id,user);
         return user;
     }
 
-    @Autowired
-    private IUserService userService;
 }
