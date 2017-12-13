05ed6f8ac286ade4954b50d30869690a4bdeee17
hexiaofei <hexiaofei4613@minxinjituan.cn>
2017-12-13 17:07:46 +0800

    consumer  增加 hystrix-dashboard

--- spring-cloud-consumer/src/main/java/com/hexiaofei/springcloudconsumer/SpringCloudConsumerApplication.java
+++ spring-cloud-consumer/src/main/java/com/hexiaofei/springcloudconsumer/SpringCloudConsumerApplication.java
@@ -2,15 +2,19 @@ package com.hexiaofei.springcloudconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class SpringCloudConsumerApplication {
 
 	@Bean
