0e0f4b0c55c39f2c66825a2b0d3316559e37c390
hexiaofei <hexiaofei4613@minxinjituan.cn>
2017-12-14 15:55:28 +0800

    增加  hystrix 仪表监控服务

---
+++ hystrix-dashboard-server/src/main/java/com/hexiaofei/HystrixDashboardServerApplication.java
@@ -0,0 +1,12 @@
+package com.hexiaofei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixDashboardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardServerApplication.class, args);
+	}
+}
