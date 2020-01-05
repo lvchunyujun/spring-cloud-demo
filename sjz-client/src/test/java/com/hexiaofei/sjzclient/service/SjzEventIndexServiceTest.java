1fb2487214e61b917d6b3bb7a946cf7722de0826
lcyj <1097957468@qq.com>
2020-01-05 21:24:01 +0800

    修改单元测试

---
+++ sjz-client/src/test/java/com/hexiaofei/sjzclient/service/SjzEventIndexServiceTest.java
@@ -0,0 +1,16 @@
+package com.hexiaofei.sjzclient.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SjzEventIndexServiceTest {
+
+    @Test
+    public void addEventIndexAndUser() {
+    }
+
+    @Test
+    public void getPageVoObjectBySjzEventIndex() {
+    }
+}
