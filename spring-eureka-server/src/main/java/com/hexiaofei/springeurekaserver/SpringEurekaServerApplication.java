8a2ce0d20be539f8402b068acb6ae6b66a676c54
lcyj <1097957468@qq.com>
2020-01-13 23:50:23 +0800

    POM.xml 排除loggin依赖

--- spring-eureka-server/src/main/java/com/hexiaofei/springeurekaserver/SpringEurekaServerApplication.java
+++ spring-eureka-server/src/main/java/com/hexiaofei/springeurekaserver/SpringEurekaServerApplication.java
@@ -1,5 +1,7 @@
 package com.hexiaofei.springeurekaserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -8,7 +10,12 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class SpringEurekaServerApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringEurekaServerApplication.class);
+
 	public static void main(String[] args) {
+		LOGGER.info("Eureka-Server start……");
 		SpringApplication.run(SpringEurekaServerApplication.class, args);
 	}
+
+
 }
